a132ba9a3cff01bc06dc08cff2af1a468edd6a50
cyberPelican <hostlockdown@gmail.com>
2020-07-18 21:20:39 +0800

    aliyun VoD

--- guli_parent/service/service_oss/src/main/java/com/chz/OssApplication.java
+++ guli_parent/service/service_oss/src/main/java/com/chz/OssApplication.java
@@ -3,11 +3,13 @@ package com.chz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 由于该模块继承service模块,所以也会继承数据库的依赖,springboot会autoConfig数据库的相关配置
  * 所以需要排除数据库的自动配置
  */
+@EnableDiscoveryClient
 //@SpringBootApplication(scanBasePackages = "com.chz")
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class OssApplication {
